2110cafeed441406f47c48736e05b074f3f8bbce
JerecTharen <kf7ags@gmail.com>
2022-12-06 21:15:10 -0500

    JT - Day 5 part 2 complete

--- Day5/jt-typescript/src/cli.ts
+++ Day5/jt-typescript/src/cli.ts
@@ -12,4 +12,10 @@ readFile('./ignore_data/input.txt', {encoding: 'utf-8'})
         const crane = new Crane(parsedFile);
         crane.followInstructions();
         console.log(`Top crates after crane instructions: ${crane.getTopCrates()}`);
+
+        console.log(`Simulating Mover 9001 . . .`);
+        const mover9001Parse = new Parser(file);
+        const mover9001 = new Crane(mover9001Parse, true);
+        mover9001.followInstructions();
+        console.log(`Mover 9001 receives top crates of: ${mover9001.getTopCrates()}`);
     })
