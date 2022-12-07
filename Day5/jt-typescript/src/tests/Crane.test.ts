8d54cfb85146cc4762c1a6b32dda401e1b0578f7
JerecTharen <kf7ags@gmail.com>
2022-12-06 21:04:06 -0500

    JT - Day 5 typescript part 1 done

---
+++ Day5/jt-typescript/src/tests/Crane.test.ts
@@ -0,0 +1,25 @@
+import Parser from "../lib/Parser";
+import Crane from "../lib/Crane";
+
+describe('Crane', ()=>{
+    const TEST_INPUT = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+`;
+    it('should be able to follow instructions and produce the expected result from the example input', ()=>{
+        const EXPECTED_TOP_STACKS = 'CMZ';
+
+        let stubParser = new Parser(TEST_INPUT);
+        let fakeCrane = new Crane(stubParser);
+        fakeCrane.followInstructions();
+        let actualTopStacks = fakeCrane.getTopCrates();
+
+        expect(actualTopStacks).toBe(EXPECTED_TOP_STACKS);
+    });
+});
